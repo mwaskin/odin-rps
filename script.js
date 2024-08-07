ff16b1d88a749e7a0b1ecdaf31a7e9d0702b2af1
Matthew Waskin <matthew@waskin.com>
2024-08-07 11:53:09 -0400

    add pseudocode to assign human choice

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // create choices array ['ROCK','PAPER','SCISSORS']
 const choices = ["ROCK", "PAPER", "SCISSORS"];
+
 // create function getComputerChoice to assign computerChoice value
 function getComputerChoice() {
 	// randomly select number 0-2 with Math.floor(Math.random() * 3)
-	// return array value whose index corresponds with random number
+	// return choice array value whose index corresponds with random number
 	return choices[Math.floor(Math.random() * 3)];
 }
 
@@ -12,3 +13,11 @@ let computerChoice = getComputerChoice();
 
 // TEST: log computerChoice
 console.log("Computer choice: " + computerChoice);
+
+// create function getHumanChoice
+// prompt user to enter ROCK, PAPER, or SCISSORS
+// return input value coverted to upperCase
+
+// create variable humanChoice and assign it return value of getHumanChoice
+
+// TEST: log humanChoice
