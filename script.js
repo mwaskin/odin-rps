e8418a4038043d167500916c5682298df3a8cc9a
Matthew Waskin <matthew@waskin.com>
2024-08-07 10:51:22 -0400

    add function to randomly select computer choice

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // create function getComputerChoice to assign computerChoice value
-// create choices array ['ROCK','PAPER','SCISSORS']
-// randomly select number 0-2 with Math.floor(Math.random() * 3)
-// return array value whose index corresponds with random number
+function getComputerChoice() {
+	// create choices array ['ROCK','PAPER','SCISSORS']
+	const choices = ["ROCK", "PAPER", "SCISSORS"];
+	// randomly select number 0-2 with Math.floor(Math.random() * 3)
+	// return array value whose index corresponds with random number
+	return choices[Math.floor(Math.random() * 3)];
+}
 
 // create variable computerChoice and assign it return value of getComputerChoice
+let computerChoice = getComputerChoice();
 
 // TEST: log computerChoice
+console.log("Computer choice: " + computerChoice);
