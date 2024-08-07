08ced09d78bb4bfbd307bb5094c58b4b4932ff41
Matthew Waskin <matthew@waskin.com>
2024-08-07 10:42:45 -0400

    add pseudocode for getting computer choice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+// create function getComputerChoice to assign computerChoice value
+// create choices array ['ROCK','PAPER','SCISSORS']
+// randomly select number 0-2 with Math.floor(Math.random() * 3)
+// return array value whose index corresponds with random number
+
+// create variable computerChoice and assign it return value of getComputerChoice
+
+// TEST: log computerChoice
